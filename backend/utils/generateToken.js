a4d3e45cec2f36a3da2f5e55de114c05ac0159be
Sagar Galave <sagargalave5@gmail.com>
2021-07-12 12:38:25 +0530

    Initial commit

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,28 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  public
+const authUser = asyncHandler(async (req, res) => {
+  // Get data from request body
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401); // unauthorized
+    throw new Error('Invalid email or password');
+  }
+});
+
+export { authUser };
